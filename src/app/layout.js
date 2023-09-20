f3c5eb2a8e9ac4687d3d1bf70644b7069623429d
Bilal Husssain <bilal.mehfooz@soc-solution.com>
2023-09-20 03:17:49 -0700

    comment script3

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,8 +16,8 @@ export default function RootLayout({ children }) {
         {children}
 
         <script src="./js/jquery.min.js"></script>
-        {/* <script src="./js/main.js"></script> */}
-        <script src="./js/particles.min.js"></script>
+        <script src="./js/main.js"></script>
+        {/* <script src="./js/particles.min.js"></script> */}
       </body>
     </html>
   );
